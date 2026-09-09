c5c27430b14221169cd134faafb418b09bec1e54
nikitamakiel1-beep <nikitamakiel1@gmail.com>
2026-09-09 13:20:57 +0200

    feat(creixement): export portfolio allocator

--- cloud/creixement-agent-core/src/index.ts
+++ cloud/creixement-agent-core/src/index.ts
@@ -2,6 +2,7 @@ export * from "./types.js";
 export * from "./scoring.js";
 export * from "./policy.js";
 export * from "./evolution.js";
+export * from "./portfolio.js";
 export * from "./runtime.js";
 export * from "./childAgents.js";
 export * from "./connectors.js";
