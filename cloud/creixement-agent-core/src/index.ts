9f5941bf0e1ee5a3d80f7e1513635eb86f9de63d
nikitamakiel1-beep <nikitamakiel1@gmail.com>
2026-09-09 13:19:14 +0200

    feat(creixement): export connector truth gates

--- cloud/creixement-agent-core/src/index.ts
+++ cloud/creixement-agent-core/src/index.ts
@@ -4,4 +4,5 @@ export * from "./policy.js";
 export * from "./evolution.js";
 export * from "./runtime.js";
 export * from "./childAgents.js";
+export * from "./connectors.js";
 export * from "./orchestrator.js";
