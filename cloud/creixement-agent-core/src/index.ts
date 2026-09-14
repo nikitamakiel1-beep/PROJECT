cbd7c2f3126982aee05ed8bdb8a2a0ab82e138fe
nikitamakiel1-beep <nikitamakiel1@gmail.com>
2026-09-14 15:03:15 +0200

    feat(v9): export Conway adversarial ecology primitives

--- cloud/creixement-agent-core/src/index.ts
+++ cloud/creixement-agent-core/src/index.ts
@@ -3,6 +3,7 @@ export * from "./scoring.js";
 export * from "./policy.js";
 export * from "./evolution.js";
 export * from "./bioecology.js";
+export * from "./conwayEcology.js";
 export * from "./portfolio.js";
 export * from "./runtime.js";
 export * from "./childAgents.js";
